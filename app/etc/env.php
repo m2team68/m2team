dbcba13a13d19ae799b76aa2d4a6ecb45fcfa2e9
M2Team <m2team68@gmail.com>
2016-06-23 23:42:59 +0700

    Commit all

---
+++ app/etc/env.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'b689d2a770748f578c52dfbc32f17a5e',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'm2teamcore',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Tue, 21 Jun 2016 15:10:54 +0000',
+  ),
+);
